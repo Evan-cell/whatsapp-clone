a6efad2a0239c78cc218e4f198e1fd275f3b93dd
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-10 15:08:01 +0300

    router-dom

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,6 +1,11 @@
 
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat'
@@ -10,10 +15,20 @@ function App() {
     <div className="app">
       
       <div className='app__body'>
+        <Router>
+          <Switch>
+          <Sidebar />
         {/* sidebar */}
-        <Sidebar />
-        {/* chat */}
+        <Route path='/rooms/:roomId'>
         <Chat />
+          </Route>
+        
+          <Route path='/'>
+        <Chat />
+          </Route>
+          </Switch>
+        </Router>
+        
         </div>
     </div>
   );
